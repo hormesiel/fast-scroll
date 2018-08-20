84df2c6f6b576ea58d9e6dbd7b8ab4710af58739
Mickaël Allonneau <mickael.allonneau@gmail.com>
2018-08-20 15:47:52 +0200

    [new] text field disabled until settings values are loaded

--- settings/index.mjs
+++ settings/index.mjs
@@ -5,5 +5,10 @@ document.addEventListener('DOMContentLoaded', () => {
   const buttonRipple = new MDCRipple(document.querySelector('.mdc-button'));
   const textField = new MDCTextField(document.querySelector('.mdc-text-field'));
 
-  console.log(chrome.storage);
+  chrome.storage.sync.get({
+    scrollFactor: 3
+  }, function(items) {
+    textField.value = items.scrollFactor;
+    textField.disabled = false;
+  });
 });
