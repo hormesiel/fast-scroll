beaba3ccb128ef06b855f3c157cf7d253ea686d1
Mickaël Allonneau <mickael.allonneau@gmail.com>
2018-08-27 19:10:56 +0200

    Refactoring: locales' fields naming

--- settings/index.mjs
+++ settings/index.mjs
@@ -4,14 +4,14 @@ import {MDCTextField} from '@material/textfield';
 document.addEventListener('DOMContentLoaded', () => {
   // (page title)
   const title = document.querySelector('#title');
-  title.innerHTML = chrome.i18n.getMessage('settingsTitle');
+  title.innerHTML = chrome.i18n.getMessage('Settings_title');
 
 
   // Scroll speed multiplier
   const scrollSpeedMultiplierTextField = new MDCTextField(document.querySelector('.mdc-text-field'));
 
   const scrollSpeedMultiplierLabel = document.querySelector('#scroll-speed-multiplier-label');
-  scrollSpeedMultiplierLabel.innerHTML = chrome.i18n.getMessage('settingsTextFieldLabel');
+  scrollSpeedMultiplierLabel.innerHTML = chrome.i18n.getMessage('Settings_ScrollSpeedMultiplier_label');
 
 
 
@@ -19,7 +19,7 @@ document.addEventListener('DOMContentLoaded', () => {
   const keyToPressTextField = new MDCTextField(document.querySelectorAll('.mdc-text-field')[1]);
 
   const keyToPressLabel = document.querySelector('#key-to-press-label');
-  keyToPressLabel.innerHTML = chrome.i18n.getMessage('settingsKeyToPressLabel');
+  keyToPressLabel.innerHTML = chrome.i18n.getMessage('Settings_KeyToPress_label');
 
   const keyToPressInput = document.querySelector('#key-to-press');
   keyToPressInput.addEventListener('keydown', (event) => {
@@ -34,7 +34,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
   // (save button)
   const button = document.querySelector('#button');
-  button.innerHTML = chrome.i18n.getMessage('settingsSaveButtonLabel');
+  button.innerHTML = chrome.i18n.getMessage('Settings_Button_label');
   button.addEventListener('click', () => {
     chrome.storage.sync.set({
       scrollSpeedMultiplier: scrollSpeedMultiplierTextField.value, // No need to cast it to a Number for multiplication to work
