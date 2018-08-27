7bff3d36c7a4bf15c60e1cffcf1a0c31a1d6952e
Mickaël Allonneau <mickael.allonneau@gmail.com>
2018-08-27 18:20:27 +0200

    Refactoring

--- settings/index.mjs
+++ settings/index.mjs
@@ -2,43 +2,56 @@ import {MDCRipple} from '@material/ripple';
 import {MDCTextField} from '@material/textfield';
 
 document.addEventListener('DOMContentLoaded', () => {
-  const buttonRipple = new MDCRipple(document.querySelector('.mdc-button'));
-  const textfieldScrollMultiplier = new MDCTextField(document.querySelector('.mdc-text-field'));
-  const textfieldTriggerKey = new MDCTextField(document.querySelectorAll('.mdc-text-field')[1]);
-
-  chrome.storage.sync.get({
-    scrollFactor: 3,
-    keyToPress: 'ShiftLeft',
-  }, function(items) {
-    textfieldScrollMultiplier.value = items.scrollFactor;
-    textfieldScrollMultiplier.disabled = false;
-
-    textfieldTriggerKey.value = items.keyToPress;
-    textfieldTriggerKey.disabled = false;
-  });
-
-  document.querySelector('.mdc-button').addEventListener('click', () => {
-    chrome.storage.sync.set({
-      scrollFactor: textfieldScrollMultiplier.value, // No need to cast it to a Number for multiplication to work
-      keyToPress: textfieldTriggerKey.value
-    });
-  });
-
+  // (page title)
   const title = document.querySelector('#title');
-  const textfieldScrollMultiplierLabel = document.querySelector('#scroll-multiplier-label');
-  const button = document.querySelector('#button');
-
   title.innerHTML = chrome.i18n.getMessage('settingsTitle');
-  textfieldScrollMultiplierLabel.innerHTML = chrome.i18n.getMessage('settingsTextFieldLabel');
-  button.innerHTML = chrome.i18n.getMessage('settingsSaveButtonLabel');
 
-  const triggerKeyInput = document.querySelector('#key-to-press');
-  triggerKeyInput.addEventListener('keydown', (event) => {
+
+  // Scroll speed multiplier
+  const scrollSpeedMultiplierTextField = new MDCTextField(document.querySelector('.mdc-text-field'));
+
+  const scrollSpeedMultiplierLabel = document.querySelector('#scroll-multiplier-label');
+  scrollSpeedMultiplierLabel.innerHTML = chrome.i18n.getMessage('settingsTextFieldLabel');
+
+
+
+  // Key to press
+  const keyToPressTextField = new MDCTextField(document.querySelectorAll('.mdc-text-field')[1]);
+
+  const keyToPressInput = document.querySelector('#key-to-press');
+  keyToPressInput.addEventListener('keydown', (event) => {
     event.preventDefault();
 
     if (event.code == 'ControlLeft'
       || event.code == 'ShiftLeft'
       || event.code == 'AltLeft')
-      triggerKeyInput.value = event.code;
+      keyToPressInput.value = event.code;
+  });
+
+
+  // (save button)
+  const button = document.querySelector('#button');
+  button.innerHTML = chrome.i18n.getMessage('settingsSaveButtonLabel');
+  button.addEventListener('click', () => {
+    chrome.storage.sync.set({
+      scrollFactor: scrollSpeedMultiplierTextField.value, // No need to cast it to a Number for multiplication to work
+      keyToPress: keyToPressTextField.value
+    });
+  });
+
+  MDCRipple.attachTo(button);
+
+
+  // (display saved/default options values)
+
+  chrome.storage.sync.get({
+    keyToPress: 'ShiftLeft',
+    scrollFactor: 3,
+  }, function(options) {
+    scrollSpeedMultiplierTextField.value = options.scrollFactor;
+    scrollSpeedMultiplierTextField.disabled = false;
+
+    keyToPressTextField.value = options.keyToPress;
+    keyToPressTextField.disabled = false;
   });
 });
