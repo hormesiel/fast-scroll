3836d6170c18987a50dc0cc9e96175d105269b86
Mickaël Allonneau <mickael.allonneau@gmail.com>
2020-05-21 16:00:13 +0200

    rewrite index script
    - fixes console error related to event.preventDefault()
    - fixes broken scrolling when using AltLeft as trigger key
    - fixes Chrome's zoom changes not prevented when using CtrlLeft as
    trigger key
    - removed ShiftLeft key support ; it's not possible to use it as trigger
    key anymore since it clashes with Chrome's horizontal scrolling

--- index.ts
+++ index.ts
@@ -2,12 +2,13 @@ declare const chrome;
 
 import defaultSettings from './default-settings';
 
-let fastScrollEnabled = false;
+let performHorizontalScroll = false;
+let pressedKeys = [];
 let settings;
 
-// Get settings values
+// Get current settings and listen to changes (when saving on the 'Options' page)
 
-chrome.storage.sync.get(defaultSettings, items => settings = items);
+chrome.storage.sync.get(defaultSettings, savedSettings => settings = savedSettings);
 
 chrome.storage.onChanged.addListener(changes => {
   if (changes.keyToPress)
@@ -16,26 +17,56 @@ chrome.storage.onChanged.addListener(changes => {
     settings.scrollSpeedMultiplier = changes.scrollSpeedMultiplier.newValue;
 });
 
-// Attach event listeners
+// Listen to mouse wheel events to trigger custom scroll behavior
 
 window.addEventListener('wheel', event => {
-  if (!fastScrollEnabled)
+  // If the trigger key isn't pressed, pass
+  if (!pressedKeys.includes(settings.keyToPress))
     return;
 
+  // Don't let the browser perform the scrolling
   event.preventDefault();
-  window.scrollBy(0, event.deltaY * settings.scrollSpeedMultiplier);
+
+  /* When scrolling to the bottom or to the right, `event.deltaY` will be a positive int ;
+  when scrolling to the top or to the left, it will be a negative int.
+  During my tests `event.deltaX` never changed and was always `0`, but I think it's because my mouse has a
+  unidirectional wheel, while some other mouses can be have a bidirectional wheel, that's why I'm checking it to support
+  all possible use cases */
+  const scrollAmountDefault = (event.deltaY || event.deltaX);
+  const scrollAmountCustom = scrollAmountDefault * settings.scrollSpeedMultiplier;
+
+  // Perform our custom scroll with a custom amount
+  if (performHorizontalScroll)
+    window.scrollBy(scrollAmountCustom, 0);
+  else
+    window.scrollBy(0, scrollAmountCustom);
+}, {
+  passive: false, // required to be able to call `event.preventDefault()`
 });
 
+// Keep a list of currently pressed keys
+
 window.addEventListener('keydown', event => {
-  if (event.code == settings.keyToPress) {
-    event.preventDefault();
-    fastScrollEnabled = true;
-  }
+  if (!pressedKeys.includes(event.code))
+    pressedKeys.push(event.code);
+
+  // Perform horizontal scroll when any 'Shift' key is pressed, vertical scroll otherwise (like Chrome's default behavior)
+  performHorizontalScroll = pressedKeys.includes('ShiftLeft') || pressedKeys.includes('ShiftRight');
 });
 
 window.addEventListener('keyup', event => {
-  if (event.code == settings.keyToPress) {
-    event.preventDefault();
-    fastScrollEnabled = false;
+  if (pressedKeys.includes(event.code)) {
+    const keyIndex = pressedKeys.indexOf(event.code);
+    pressedKeys.splice(keyIndex, 1);
   }
+
+  // Perform horizontal scroll when any 'Shift' key is pressed, vertical scroll otherwise (like Chrome's default behavior)
+  performHorizontalScroll = pressedKeys.includes('ShiftLeft') || pressedKeys.includes('ShiftRight');
 });
+
+// Clear all pressed keys when the page loses focus
+// (by default keys stay pressed when tab-switching to another OS window)
+
+window.addEventListener('blur', () => pressedKeys = []);
+
+window.addEventListener('debug', () => console.log(pressedKeys));
