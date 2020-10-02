016903f823f4a688ab25410ee3cd3afb5a24d3dd
juleshwar.babu <juleshwar.babu@zohocorp.com>
2020-10-02 08:45:53 +0530

    Fixed two issues in build.sh Copying both res and libs as folders instead of just the subtree files. This fixed errors while loading the _build folder as an unpacked extension

--- build.sh
+++ build.sh
@@ -9,7 +9,7 @@ mkdir $builddir
 
 # Copy main files
 cp $srcdir/manifest.json $builddir
-cp -r $srcdir/res/ $builddir
+cp -r $srcdir/res $builddir
 cp -r $srcdir/_locales $builddir
 
 # Copy settings files
@@ -17,7 +17,7 @@ mkdir $builddir/settings
 cp $srcdir/settings/index.html $builddir/settings/
 
 # Copy libs files
-cp -r libs/ $builddir
+cp -r libs $builddir
 
 # Compile code
 node node_modules/rollup/dist/bin/rollup -c $1 # '$1' equals '--watch' when provided
